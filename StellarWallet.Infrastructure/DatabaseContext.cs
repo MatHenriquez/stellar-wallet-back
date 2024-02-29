9da737535aa1afa13d4b813a951789ec1a8c8fc5
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-02-28 23:53:10 -0300

    Add error handling when getting users

--- StellarWallet.Infrastructure/DatabaseContext.cs
+++ StellarWallet.Infrastructure/DatabaseContext.cs
@@ -1,7 +1,8 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using StellarWallet.Domain.Entities;
 
-namespace StellarWallet.Infrastructure
+namespace StellarWallet.Infrastructure.DatabaseConnection
 {
     public class DatabaseContext : DbContext
     {
@@ -9,8 +10,15 @@ namespace StellarWallet.Infrastructure
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Server=(localdb)\\CFServer;Database=StellarWallet;Trusted_Connection=True;Encrypt=False;TrustServerCertificate=True");
-        }
+            var configuration = new ConfigurationBuilder()
+              .SetBasePath(Directory.GetCurrentDirectory())
+              .AddJsonFile("appsettings.json", optional: false)
+              .Build();
 
+            var path = Directory.GetCurrentDirectory();
+
+            var connectionString = configuration.GetConnectionString("StellarWallet");
+            optionsBuilder.UseSqlServer(connectionString);
+        }
     }
 }
