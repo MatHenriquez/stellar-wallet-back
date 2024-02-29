1b0a5202cf736892d83c568ae3df0749025cfc61
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-02-28 23:51:28 -0300

    Install and configure AutoMapper

--- StellarWallet.WebApi/Program.cs
+++ StellarWallet.WebApi/Program.cs
@@ -1,11 +1,25 @@
+using StellarWallet.Application.Interfaces;
+using StellarWallet.Application.Mappers;
+using StellarWallet.Application.Services;
+using StellarWallet.Domain.Repositories;
+using StellarWallet.Infrastructure.DatabaseConnection;
+using StellarWallet.Infrastructure.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddDbContext<DatabaseContext>(); // Add DatabaseContext
+builder.Services.AddScoped<IUserRepository, UserRepository>(); // Add UserRepository
+builder.Services.AddScoped<IUserService, UserService>(); // Add UserService
+builder.Services.AddAutoMapper(typeof(AutoMapperProfile)); // Add AutoMapper
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
 
 var app = builder.Build();
 
