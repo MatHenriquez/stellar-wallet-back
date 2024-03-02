874a35535813a2836e9900e9130cd53220d0515d
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-03-02 18:40:51 -0300

    Rename Repositories folder to Interfaces and implement IBlckchainService interface

---
+++ StellarWallet.Domain/Entities/StellarAccount.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StellarWallet.Domain.Entities
+{
+    public class StellarAccount(string PublicKey, string SecretKey)
+    {
+        [Required(ErrorMessage = "Public key is required")]
+        [StringLength(56, MinimumLength = 56, ErrorMessage = "Public key must have 56 characters")]
+        public string PublicKey { get; set; } = PublicKey;
+
+        [Required(ErrorMessage = "Secret key is required")]
+        [StringLength(56, MinimumLength = 56, ErrorMessage = "Secret key must have 56 characters")]
+        public string SecretKey { get; set; } = SecretKey;
+    }
+}
