0c5d288f6eea9a1fbbe9d7dd00d3202514d57ab4
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-03-02 23:57:07 -0300

    Change CORS policy to admit requests from any URL

--- StellarWallet.WebApi/Program.cs
+++ StellarWallet.WebApi/Program.cs
@@ -21,10 +21,19 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowBlazor", builder =>
+    {
+        builder.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+    });
+});
 
 var app = builder.Build();
+
+app.UseCors("AllowBlazor");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
