ae1333b00c7e75b01399f4a7b5931b5c407e7a47
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-03-10 18:55:54 -0300

    Implement login feature

---
+++ StellarWallet.Application/Dtos/Requests/LoginDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StellarWallet.Application.Dtos.Requests
+{
+    public class LoginDto(string email, string password)
+    {
+        [EmailAddress(ErrorMessage = "Must be a valid email address"), Required(ErrorMessage = "Email must be provided")]
+        public string Email { get; set; } = email;
+
+        [Required(ErrorMessage = "Password must be provided")]
+        [Length(8, 50, ErrorMessage = "Password must have between 8 and 50 characters")]
+        public string Password { get; set; } = password;
+    }
+}
---
+++ StellarWallet.Application/Services/AuthService.cs
@@ -0,0 +1,23 @@
+﻿using StellarWallet.Application.Dtos.Requests;
+using StellarWallet.Application.Dtos.Responses;
+using StellarWallet.Application.Interfaces;
+using StellarWallet.Domain.Entities;
+using StellarWallet.Domain.Repositories;
+
+namespace StellarWallet.Application.Services
+{
+    public class AuthService(IUserRepository userRepository) : IAuthService
+    {
+        private readonly IUserRepository _userRepository = userRepository;
+
+        public async Task<LoggedDto> Login(LoginDto loginDto)
+        {
+            User user = await _userRepository.GetBy("Email", loginDto.Email);
+
+            if (user.Password != loginDto.Password)
+                throw new Exception("Invalid password");
+
+            return new LoggedDto(true, "token");
+        }
+    }
+}
