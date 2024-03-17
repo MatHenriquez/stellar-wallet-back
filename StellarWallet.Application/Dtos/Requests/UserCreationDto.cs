8a2a4c5b3b9c144631923fc22beff55391570787
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-03-17 11:11:58 -0300

    Update UserCreationDto

--- StellarWallet.Application/Dtos/Requests/UserCreationDto.cs
+++ StellarWallet.Application/Dtos/Requests/UserCreationDto.cs
@@ -5,13 +5,13 @@ namespace StellarWallet.Application.Dtos.Requests
     public class UserCreationDto(string Name, string LastName, string Email, string Password, string? PublicKey, string? SecretKey)
     {
         [Required(ErrorMessage = "Name is required")]
-        [StringLength(50, MinimumLength = 6, ErrorMessage = "Name must have a maximum of 50 characters and a minimun of 6.")]
-        [RegularExpression(@"^[a-zA-Z''-'\s]{1,50}$", ErrorMessage = "Special characters are not allowed.")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Name must have a maximum of 50 characters and a minimun of 3.")]
+        [RegularExpression(@"^[a-zA-Z''-'\s]{3,50}$", ErrorMessage = "Special characters are not allowed.")]
         public string Name { get; set; } = Name;
 
         [Required(ErrorMessage = "Lastname is required")]
-        [StringLength(50, MinimumLength = 6, ErrorMessage = "Lastname must have a maximum of 50 characters and a minimun of 6.")]
-        [RegularExpression(@"^[a-zA-Z''-'\s]{1,50}$", ErrorMessage = "Special characters are not allowed.")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Lastname must have a maximum of 50 characters and a minimun of 3.")]
+        [RegularExpression(@"^[a-zA-Z''-'\s]{3,50}$", ErrorMessage = "Special characters are not allowed.")]
         public string LastName { get; set; } = LastName;
 
         [Required(ErrorMessage = "Email is required")]
@@ -23,11 +23,11 @@ namespace StellarWallet.Application.Dtos.Requests
         public string Password { get; set; } = Password;
 
         [StringLength(56, MinimumLength = 56, ErrorMessage = "Public key must have 56 characters.")]
-        [RegularExpression(@"^[a-zA-Z''-'\s]{1,57}$", ErrorMessage = "Special characters are not allowed.")]
+        [RegularExpression(@"^[a-zA-Z''-'\s]{56}$", ErrorMessage = "Special characters are not allowed.")]
         public string? PublicKey { get; set; } = PublicKey;
 
         [StringLength(56, MinimumLength = 56, ErrorMessage = "Secret key must have 56 characters.")]
-        [RegularExpression(@"^[a-zA-Z''-'\s]{1,57}$", ErrorMessage = "Special characters are not allowed.")]
+        [RegularExpression(@"^[a-zA-Z''-'\s]{56}$", ErrorMessage = "Special characters are not allowed.")]
         public string? SecretKey { get; set; } = SecretKey;
     } 
 }
