1bfab0c5d174e702532d8acb58f5d971fa5d5743
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-04-27 20:00:30 -0300

    Update the LoggedDto class

--- StellarWallet.Application/Dtos/Responses/LoggedDto.cs
+++ StellarWallet.Application/Dtos/Responses/LoggedDto.cs
@@ -1,8 +1,9 @@
 ﻿namespace StellarWallet.Application.Dtos.Responses
 {
-    public class LoggedDto(bool success, string token)
+    public class LoggedDto(bool success, string token, string? publicKey)
     {
         public bool Success { get; set; } = success;
-        public string Token { get; set; } = token;
+        public string? Token { get; set; } = token;
+        public string? PublicKey { get; set; } = publicKey;
     }
 }
