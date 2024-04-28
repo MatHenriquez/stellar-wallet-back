f4fda2a9cba6ce54a5c45003afe2a88dbd68a798
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-04-28 12:41:03 -0300

    Implement the EncryptionService class and its interface

---
+++ StellarWallet.Application/Interfaces/IEncryptionService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StellarWallet.Application.Interfaces
+{
+    public interface IEncryptionService
+    {
+        string Encrypt(string text);
+        bool Verify(string text, string hash);
+    }
+}
