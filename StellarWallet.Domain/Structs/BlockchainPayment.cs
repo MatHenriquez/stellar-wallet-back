db83079f207fe24f84235ef553e18b220f9d1082
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-04-30 23:13:03 -0300

    Update the IBlockchainService interface to include the GetPayements method

---
+++ StellarWallet.Domain/Structs/BlockchainPayment.cs
@@ -0,0 +1,14 @@
+﻿namespace StellarWallet.Domain.Structs
+{
+    public struct BlockchainPayment
+    {
+        public string Id { get; set; }
+        public string From { get; set; }
+        public string To { get; set; }
+        public string Amount { get; set; }
+        public string Asset { get; set; }
+        public string CreatedAt { get; set; }
+        public bool WasSuccessful { get; set; }
+    }
+}
+
