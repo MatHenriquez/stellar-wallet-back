1ecc7e9c2e138792cc4a78787bc3be2d3e6d8162
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-04-30 23:14:33 -0300

    Implement the GetPayments method in the Stellar service

--- StellarWallet.Infrastructure/Stellar/Stellar.cs
+++ StellarWallet.Infrastructure/Stellar/Stellar.cs
@@ -1,7 +1,9 @@
 ﻿using stellar_dotnet_sdk;
 using stellar_dotnet_sdk.responses;
+using stellar_dotnet_sdk.responses.operations;
 using StellarWallet.Domain.Entities;
 using StellarWallet.Domain.Repositories;
+using StellarWallet.Domain.Structs;
 
 namespace StellarWallet.Infrastructure.Stellar
 {
@@ -43,6 +45,49 @@ namespace StellarWallet.Infrastructure.Stellar
             {
                 throw new Exception("Stellar Error " + e.Message);
             }
+        }
+
+        public async Task<BlockchainPayment[]> GetPayments(string accountId)
+        {
+            var payments = new List<BlockchainPayment>();
+
+            var server = new Server("https://horizon-testnet.stellar.org");
+            var paymentsRequestBuilder = server.Payments.ForAccount(accountId);
+
+            try
+            {
+                var page = await paymentsRequestBuilder.Execute();
+
+                while (true)
+                {
+                    foreach (var record in page.Records.OfType<PaymentOperationResponse>())
+                    {
+                        payments.Add(new BlockchainPayment
+                        {
+                            Id = record.TransactionHash,
+                            From = record.From,
+                            To = record.To,
+                            Amount = record.Amount,
+                            Asset = record.Asset is AssetTypeNative ? "native" : ((AssetTypeCreditAlphaNum)record.Asset).Code,
+                            CreatedAt = record.CreatedAt.ToString(),
+                            WasSuccessful = record.TransactionSuccessful
+                        });
+                    }
+
+                    if (page.Records.Count == 0)
+                    {
+                        break;
+                    }
+
+                    page = await paymentsRequestBuilder.Cursor(page.Records.Last().PagingToken).Execute();
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Stellar Error " + e.Message);
+            }
+
+            return payments.ToArray();
         }
     }
 }
