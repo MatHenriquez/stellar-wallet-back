17123e923eb36897fb50cefa3737e13a1c651e96
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-05-04 19:09:36 -0300

    Implement a new transaction endpoint for test funding

---
+++ StellarWallet.Application/Dtos/Requests/GetTestFundsDto.cs
@@ -0,0 +1,7 @@
+﻿namespace StellarWallet.Application.Dtos.Requests
+{
+    public class GetTestFundsDto(string publicKey)
+    {
+        public string PublicKey { get; set; } = publicKey;
+    }
+}
