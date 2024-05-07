854a61b319987e0831d9a9e874ecafaaef895107
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-05-06 21:19:31 -0300

    Relate the User and BlockchainAccount entitites

--- StellarWallet.Domain/Entities/BlockchainAccount.cs
+++ StellarWallet.Domain/Entities/BlockchainAccount.cs
@@ -2,8 +2,11 @@
 
 namespace StellarWallet.Domain.Entities
 {
-    public class BlockchainAccount(string PublicKey, string SecretKey)
+    public class BlockchainAccount(string PublicKey, string SecretKey, int userId)
     {
+        [Key]
+        public int Id { get; private set; }
+
         [Required(ErrorMessage = "Public key is required")]
         [StringLength(56, MinimumLength = 56, ErrorMessage = "Public key must have 56 characters")]
         public string PublicKey { get; set; } = PublicKey;
@@ -11,5 +14,10 @@ namespace StellarWallet.Domain.Entities
         [Required(ErrorMessage = "Secret key is required")]
         [StringLength(56, MinimumLength = 56, ErrorMessage = "Secret key must have 56 characters")]
         public string SecretKey { get; set; } = SecretKey;
+
+        [Required(ErrorMessage = "User id is required")]
+        public int UserId { get; set; } = userId;
+
+        public User? User { get; set; }
     }
 }
