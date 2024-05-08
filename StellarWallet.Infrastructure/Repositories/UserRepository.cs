c12f72e3984bd3832d5d2dbbdda3bc8b2099b181
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-05-08 20:55:46 -0300

    Update the UserRepository to include the blockchain accounts

--- StellarWallet.Infrastructure/Repositories/UserRepository.cs
+++ StellarWallet.Infrastructure/Repositories/UserRepository.cs
@@ -25,19 +25,24 @@ namespace StellarWallet.Infrastructure.Repositories
 
         public async Task<IEnumerable<User>> GetAll()
         {
-            return await _context.Users.ToListAsync();
+            return await _context.Users.Include(
+                u => u.BlockchainAccounts
+                ).ToListAsync();
         }
 
         public async Task<User> GetById(int id)
         {
-            User? foundUser = await _context.Users.FindAsync(id);
+            User? foundUser = await _context.Users
+                .Include(u => u.BlockchainAccounts)
+                .FirstOrDefaultAsync(u => u.Id == id);
+
             return foundUser is null ? throw new Exception("User not found") : foundUser;
         }
 
         public async Task<User?> GetBy(string paramName, string paramValue)
         {
             var propertyInfo = typeof(User).GetProperty(paramName) ?? throw new ArgumentException($"Invalid property: '{paramName}'.");
-            var query = _context.Users.Where(u => EF.Property<string>(u, propertyInfo.Name) == paramValue);
+            var query = _context.Users.Where(u => EF.Property<string>(u, propertyInfo.Name) == paramValue).Include(u => u.BlockchainAccounts);
 
             try
             {
