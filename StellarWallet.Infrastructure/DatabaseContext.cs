81fe458503217fa7e7d9b5563b1bf9ebb44cac6a
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-05-08 20:56:24 -0300

    Update the DatabaseContext class

--- StellarWallet.Infrastructure/DatabaseContext.cs
+++ StellarWallet.Infrastructure/DatabaseContext.cs
@@ -1,5 +1,4 @@
-﻿using LaunchDarkly.EventSource;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using StellarWallet.Domain.Entities;
 
@@ -7,25 +6,34 @@ namespace StellarWallet.Infrastructure.DatabaseConnection
 {
     public class DatabaseContext : DbContext
     {
-        private readonly IConfiguration _configuration;
         public DbSet<User> Users { get; set; }
         public DbSet<BlockchainAccount> BlockchainAccounts { get; set; }
-
-        public DatabaseContext(IConfiguration configuration)
-        {
-            _configuration = configuration;
-        }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<User>()
                 .HasIndex(u => u.Email)
                 .IsUnique();
+
+            modelBuilder.Entity<User>().HasMany(u => u.BlockchainAccounts)
+                .WithOne(b => b.User)
+                .HasForeignKey(b => b.UserId);
+
+            modelBuilder.Entity<BlockchainAccount>()
+                .HasIndex(b => b.PublicKey)
+                .IsUnique();
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            var connectionString = _configuration.GetConnectionString("StellarWallet");
+            var configuration = new ConfigurationBuilder()
+              .SetBasePath(Directory.GetCurrentDirectory())
+              .AddJsonFile("appsettings.json", optional: false)
+              .Build();
+
+            var path = Directory.GetCurrentDirectory();
+            var connectionString = configuration.GetConnectionString("StellarWallet");
+
             optionsBuilder.UseSqlServer(connectionString);
         }
     }
