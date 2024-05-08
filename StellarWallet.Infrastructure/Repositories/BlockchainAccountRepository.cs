795fe362e14de835a5e27ee145e3f77d5e0802e7
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-05-08 20:55:11 -0300

    Implement the BlockchainAccountRepository and its interface

---
+++ StellarWallet.Infrastructure/Repositories/BlockchainAccountRepository.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StellarWallet.Domain.Entities;
+using StellarWallet.Domain.Interfaces;
+using StellarWallet.Infrastructure.DatabaseConnection;
+
+namespace StellarWallet.Infrastructure.Repositories
+{
+    public class BlockchainAccountRepository(DatabaseContext context) : IBlockchainAccountRepository
+    {
+        private readonly DatabaseContext _context = context;
+
+        public async Task Add(BlockchainAccount blockchainAccount)
+        {
+            try { 
+            await _context.BlockchainAccounts.AddAsync(blockchainAccount);
+            await _context.SaveChangesAsync();
+                } catch (Exception e)
+            {
+                throw new Exception("Error adding blockchain account", e);
+            }
+        }
+    }
+}
