74c00d70b8093308c3b5d503a25a6c7ebd6f06d8
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-05-08 22:01:05 -0300

    Update DatabaseContext

--- StellarWallet.Infrastructure/DatabaseContext.cs
+++ StellarWallet.Infrastructure/DatabaseContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿﻿using LaunchDarkly.EventSource;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using StellarWallet.Domain.Entities;
 
@@ -6,8 +7,14 @@ namespace StellarWallet.Infrastructure.DatabaseConnection
 {
     public class DatabaseContext : DbContext
     {
+        private readonly IConfiguration _configuration;
         public DbSet<User> Users { get; set; }
         public DbSet<BlockchainAccount> BlockchainAccounts { get; set; }
+
+        public DatabaseContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -26,29 +33,8 @@ namespace StellarWallet.Infrastructure.DatabaseConnection
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            var environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "test";
-
-            var configurationBuilder = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory());
-
-            if (File.Exists($"appsettings.{environmentName}.json"))
-            {
-                configurationBuilder.AddJsonFile($"appsettings.{environmentName}.json", optional: false);
-            } else
-            {
-                configurationBuilder.AddJsonFile("appsettings.json", optional: false);
-
-            }
-
-            var configuration = configurationBuilder.Build();
-
-            var connectionString = configuration.GetConnectionString("StellarWallet");
-
-            if (!string.IsNullOrEmpty(connectionString))
-            {
-                optionsBuilder.UseSqlServer(connectionString);
-            }
+            var connectionString = _configuration.GetConnectionString("StellarWallet");
+            optionsBuilder.UseSqlServer(connectionString);
         }
-
     }
 }
