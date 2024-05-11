23a7d7e8d66262d5183861839e0640725dbf2d55
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-05-10 21:31:56 -0300

    Implement the UserContactRepository class

---
+++ StellarWallet.Domain/Interfaces/IUserContactRepository.cs
@@ -0,0 +1,13 @@
+﻿using StellarWallet.Domain.Entities;
+
+namespace StellarWallet.Domain.Interfaces
+{
+    public interface IUserContactRepository
+    {
+        Task<IEnumerable<UserContact>> GetAll(int userId);
+        Task<UserContact> GetById(int id);
+        Task Add(UserContact userContact);
+        Task Update(UserContact userContact);
+        Task Delete(int id);
+    }
+}
---
+++ StellarWallet.Infrastructure/Repositories/UserContactRepository.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StellarWallet.Domain.Entities;
+using StellarWallet.Domain.Interfaces;
+using StellarWallet.Infrastructure.DatabaseConnection;
+
+
+namespace StellarWallet.Infrastructure.Repositories
+{
+    public class UserContactRepository(DatabaseContext context) : IUserContactRepository
+    {
+        private readonly DatabaseContext _context = context;
+
+        public async Task Add(UserContact userContact)
+        {
+            await _context.UserContacts.AddAsync(userContact);
+        }
+
+        public async Task Delete(int id)
+        {
+            UserContact? foundUserContact = await GetById(id) ?? throw new Exception("User contact not found");
+            _context.UserContacts.Remove(foundUserContact);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<UserContact> GetById(int id)
+        {
+            UserContact? foundUserContact = await _context.UserContacts.FindAsync(id);
+            return foundUserContact is null ? throw new Exception("User contact not found") : foundUserContact;
+        }
+
+        public async Task<IEnumerable<UserContact>> GetAll(int userId)
+        {
+            return await _context.UserContacts.Where(uc => uc.UserId == userId).ToListAsync();
+        }
+
+        public Task Update(UserContact userContact)
+        {
+            _context.UserContacts.Update(userContact);
+            return _context.SaveChangesAsync();
+        }
+    }
+}
