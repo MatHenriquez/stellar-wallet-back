b4c94e753751cb925c23822a11b8c927fc84baa7
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-05-10 21:35:31 -0300

    Update DataAnnotations of the DTOs

--- StellarWallet.Application/Dtos/Requests/GetBalancesDto.cs
+++ StellarWallet.Application/Dtos/Requests/GetBalancesDto.cs
@@ -1,8 +1,13 @@
-﻿namespace StellarWallet.Application.Dtos.Requests
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StellarWallet.Application.Dtos.Requests
 {
     public class GetBalancesDto
     {
+        [Required(ErrorMessage = "Public key is required")]
+        [StringLength(56, MinimumLength = 56, ErrorMessage = "Public key must have 56 characters")]
         public string PublicKey { get; set; } = string.Empty;
+
         public bool FilterZeroBalances { get; set; } = false;
 
         public GetBalancesDto() { }
