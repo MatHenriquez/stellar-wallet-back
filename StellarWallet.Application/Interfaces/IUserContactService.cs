c4304a036d9ae992a52edd09e3bacb3ee3ff9864
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-05-10 21:33:39 -0300

    Implemen the UserContactService class and its interface

---
+++ StellarWallet.Application/Interfaces/IUserContactService.cs
@@ -0,0 +1,18 @@
+﻿using StellarWallet.Application.Dtos.Requests;
+using StellarWallet.Application.Dtos.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StellarWallet.Application.Interfaces
+{
+    public  interface IUserContactService
+    {
+        Task<IEnumerable<UserContactsDto>> GetAll(int id);
+        Task Add(AddContactDto userContact);
+        Task Update(UpdateContactDto userContact);
+        Task Delete(int id);
+    }
+}
