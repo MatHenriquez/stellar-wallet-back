0ae6327374d727b8b61a3d847df85dd219eb0e29
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-05-12 11:44:28 -0300

    Create new migrations

---
+++ StellarWallet.Infrastructure/Migrations/20240512143354_UpdatePublicKeyInUserContact.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StellarWallet.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatePublicKeyInUserContact : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
