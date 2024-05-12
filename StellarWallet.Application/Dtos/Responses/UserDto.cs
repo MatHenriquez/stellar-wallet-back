deba7bf3d00870cdb2d8554bb7d9607f92e73ef8
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-05-12 11:44:54 -0300

    Fix DTOs

--- StellarWallet.Application/Dtos/Responses/UserDto.cs
+++ StellarWallet.Application/Dtos/Responses/UserDto.cs
@@ -2,13 +2,14 @@
 
 namespace StellarWallet.Application.Dtos.Responses
 {
-    public class UserDto(int Id, string name, string lastName, string email, string publicKey, ICollection<BlockchainAccount>? blockchainAccounts)
+    public class UserDto(int Id, string name, string lastName, string email, string publicKey, ICollection<BlockchainAccountDto>? blockchainAccounts, ICollection<UserContactsDto> userContacts)
     {
         public int Id { get; set; } = Id;
         public string Name { get; set; } = name; 
         public string LastName { get; set; } = lastName;
         public string Email { get; set; } = email;
         public string PublicKey { get; set; } = publicKey;
-        public ICollection<BlockchainAccount>? BlockchainAccounts { get; set; } = blockchainAccounts;
+        public ICollection<BlockchainAccountDto>? BlockchainAccounts { get; set; } = blockchainAccounts;
+        public ICollection<UserContactsDto>? UserContacts { get; set; } = userContacts;
     }
 }
