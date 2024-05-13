5ff7df92893f28a9b335136caa1ebb0a79f733ef
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-05-13 19:27:18 -0300

    Refactor the private attributes of StellarService to store their values in a configuration file

---
+++ StellarWallet.Infrastructure/Utilities/StellarConstants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StellarWallet.Infrastructure.Utilities
+{
+    public static class StellarConstants
+    {
+        public const string Section = "Stellar";
+        public const string HorizonUrl = "HorizonUrl";
+        public const string Passphrase = "Passphrase";
+    }
+}
