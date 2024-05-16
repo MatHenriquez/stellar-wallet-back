e2b8a06c947c16c2bd83dc75c9f3ec27f7288b5c
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-05-15 23:25:17 -0300

    Update LoggedDto to use nullable token

--- StellarWallet.Application/Dtos/Responses/LoggedDto.cs
+++ StellarWallet.Application/Dtos/Responses/LoggedDto.cs
@@ -1,6 +1,6 @@
 ﻿namespace StellarWallet.Application.Dtos.Responses
 {
-    public class LoggedDto(bool success, string token, string? publicKey)
+    public class LoggedDto(bool success, string? token, string? publicKey)
     {
         public bool Success { get; set; } = success;
         public string? Token { get; set; } = token;
