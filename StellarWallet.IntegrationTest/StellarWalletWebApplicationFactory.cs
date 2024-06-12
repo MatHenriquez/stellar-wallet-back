5ab84f13b1713ce6cd2f34177dd5d65fde61ad2e
Matías Henríquez <matias.henriquez.dev@gmail.com>
2024-06-12 20:15:35 -0300

    Implement the StellarWalletWebApplicationFactory class

---
+++ StellarWallet.IntegrationTest/StellarWalletWebApplicationFactory.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using StellarWallet.Infrastructure.DatabaseConnection;
+
+namespace StellarWallet.IntegrationTest
+{
+    internal class StellarWalletWebApplicationFactory : WebApplicationFactory<Program>
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureTestServices(services =>
+            {
+                services.RemoveAll(typeof(DbContextOptions<DatabaseContext>));
+                var connectionString = GetConnectionString();
+
+                services.AddDbContext<DatabaseContext>(options =>
+                {
+                    options.UseInMemoryDatabase(Guid.NewGuid().ToString());
+                });
+
+                var dbContext = CreateDatabaseContext(services);
+                dbContext.Database.EnsureCreated();
+            });
+        }
+
+        private static string? GetConnectionString()
+        {
+            var configuration = new ConfigurationBuilder()
+                .AddUserSecrets<StellarWalletWebApplicationFactory>()
+                .Build();
+
+            return configuration.GetConnectionString("StellarWalletTestDatabase");
+        }
+
+        private static DatabaseContext CreateDatabaseContext(IServiceCollection services)
+        {
+            var serviceProvider = services.BuildServiceProvider();
+            var scope = serviceProvider.CreateScope();
+            return scope.ServiceProvider.GetRequiredService<DatabaseContext>();
+        }
+    }
+}
