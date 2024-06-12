89e5d210f282ce3fd3a18fff54a939ae808ce67c
Matías Henríquez <matias.henriquez.dev@gmail.com>
2024-06-12 20:16:48 -0300

    Remove unused code in the StellarConstants.cs file

--- StellarWallet.Infrastructure/Utilities/StellarConstants.cs
+++ StellarWallet.Infrastructure/Utilities/StellarConstants.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace StellarWallet.Infrastructure.Utilities
+﻿namespace StellarWallet.Infrastructure.Utilities
 {
     public static class StellarConstants
     {
