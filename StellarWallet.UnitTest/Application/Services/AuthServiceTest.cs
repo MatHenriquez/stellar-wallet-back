f33eeaf614616c3c120dbc90eb6cd3bcb84cf94a
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-08-01 22:19:13 -0300

    Update the AuthService unit tests

--- StellarWallet.UnitTest/Application/Services/AuthServiceTest.cs
+++ StellarWallet.UnitTest/Application/Services/AuthServiceTest.cs
@@ -1,10 +1,10 @@
 ﻿using Moq;
 using StellarWallet.Application.Dtos.Requests;
 using StellarWallet.Application.Dtos.Responses;
-using StellarWallet.Application.Interfaces;
 using StellarWallet.Application.Services;
 using StellarWallet.Domain.Entities;
-using StellarWallet.Domain.Repositories;
+using StellarWallet.Domain.Interfaces.Persistence;
+using StellarWallet.Domain.Interfaces.Services;
 
 namespace StellarWallet.UnitTest.Application.Services
 {
@@ -13,17 +13,17 @@ namespace StellarWallet.UnitTest.Application.Services
         [Fact]
         public async Task When_ValidLoginDto_Expected_ReturnLoggedDto()
         {
-            var mockUserRepository = new Mock<IUserRepository>();
+            var mockUnitOfWork = new Mock<IUnitOfWork>();
             var mockJwtService = new Mock<IJwtService>();
             var mockEncryptionService = new Mock<IEncryptionService>();
 
-            var sut = new AuthService(mockUserRepository.Object, mockJwtService.Object, mockEncryptionService.Object);
+            var sut = new AuthService(mockJwtService.Object, mockEncryptionService.Object, mockUnitOfWork.Object);
 
             var loggedDto = new LoggedDto(true, "token", "GBXBDKPGYO74VVO64A7PBIHV7XN4QH4PJIRBSQT4OVE4U7JYY345PQMA");
 
             var loginDto = new LoginDto("john.doe@mail.com", "MyPassword123.");
 
-            mockUserRepository.Setup(x => x.GetBy("Email", loginDto.Email)).ReturnsAsync(new User(
+            mockUnitOfWork.Setup(x => x.User.GetBy("Email", loginDto.Email)).ReturnsAsync(new User(
                 "John",
                 "Doe",
                 "john.doe@mail.com",
@@ -46,11 +46,16 @@ namespace StellarWallet.UnitTest.Application.Services
         [Fact]
         public async Task When_InvalidLoginDto_Expected_ThrowException()
         {
-            var mockUserRepository = new Mock<IUserRepository>();
+            var mockUnitOfWork = new Mock<IUnitOfWork>();
             var mockJwtService = new Mock<IJwtService>();
             var mockEncryptionService = new Mock<IEncryptionService>();
 
-            var sut = new AuthService(mockUserRepository.Object, mockJwtService.Object, mockEncryptionService.Object);
+            var sut = new AuthService(mockJwtService.Object, mockEncryptionService.Object, mockUnitOfWork.Object);
+
+            Mock<User> user = new("John", "Doe", "john.doe@mail.com", "EncryptedPassword", "GBXBDKPGYO74VVO64A7PBIHV7XN4QH4PJIRBSQT4OVE4U7JYY345PQMA", "SC6KTRKOT33RRH2KXK2BMGJWJ7TE5NQGRE5NIWTBGNMSPLKGQ2C63KDB", "admin");
+
+
+            mockUnitOfWork.Setup(x => x.User.GetBy("Email", "john.doe@mail.com")).ReturnsAsync(user.Object);
 
             var loginDto = new LoginDto("john.doe@mail.com", "MyPassword123.");
             mockEncryptionService.Setup(x => x.Verify(loginDto.Password, "EncryptedPassword")).Returns(false);
@@ -61,14 +66,14 @@ namespace StellarWallet.UnitTest.Application.Services
         [Fact]
         public void When_UnexistingUser_Expected_ThrowException()
         {
-            var mockUserRepository = new Mock<IUserRepository>();
+            var mockUnitOfWork = new Mock<IUnitOfWork>();
             var mockJwtService = new Mock<IJwtService>();
             var mockEncryptionService = new Mock<IEncryptionService>();
 
-            var sut = new AuthService(mockUserRepository.Object, mockJwtService.Object, mockEncryptionService.Object);
+            var sut = new AuthService(mockJwtService.Object, mockEncryptionService.Object, mockUnitOfWork.Object);
 
             var loginDto = new LoginDto("john.doe@mail.com", "MyPassword123.");
-            mockUserRepository.Setup(x => x.GetBy("Email", loginDto.Email)).ReturnsAsync((User?)null);
+            mockUnitOfWork.Setup(x => x.User.GetBy("Email", loginDto.Email)).ReturnsAsync((User?)null);
 
             Assert.ThrowsAsync<Exception>(() => sut.Login(loginDto));
         }
@@ -76,11 +81,11 @@ namespace StellarWallet.UnitTest.Application.Services
         [Fact]
         public void When_ValidJwt_Expected_ReturnTrue()
         {
-            var mockUserRepository = new Mock<IUserRepository>();
+            var mockUnitOfWork = new Mock<IUnitOfWork>();
             var mockJwtService = new Mock<IJwtService>();
             var mockEncryptionService = new Mock<IEncryptionService>();
 
-            var sut = new AuthService(mockUserRepository.Object, mockJwtService.Object, mockEncryptionService.Object);
+            var sut = new AuthService(mockJwtService.Object, mockEncryptionService.Object, mockUnitOfWork.Object);
 
             var jwt = "token";
             var email = "john.doe@mail.com";
@@ -95,11 +100,11 @@ namespace StellarWallet.UnitTest.Application.Services
         [Fact]
         public void When_InvalidJwt_Expected_ThrowException()
         {
-            var mockUserRepository = new Mock<IUserRepository>();
+            var mockUnitOfWork = new Mock<IUnitOfWork>();
             var mockJwtService = new Mock<IJwtService>();
             var mockEncryptionService = new Mock<IEncryptionService>();
 
-            var sut = new AuthService(mockUserRepository.Object, mockJwtService.Object, mockEncryptionService.Object);
+            var sut = new AuthService(mockJwtService.Object, mockEncryptionService.Object, mockUnitOfWork.Object);
 
             var jwt = "token";
             var email = "john.doe@mail.com";
