e444a4c06bc59ffbe45a6b2b2c73df61620e364d
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-08-01 22:14:52 -0300

    Move the entity configurations to a new folder called Config

---
+++ StellarWallet.Infrastructure/Config/BlockchainAccountConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using StellarWallet.Domain.Entities;
+
+namespace StellarWallet.Infrastructure.Config
+{
+    public class BlockchainAccountConfiguration : IEntityTypeConfiguration<BlockchainAccount>
+    {
+        public void Configure(EntityTypeBuilder<BlockchainAccount> builder)
+        {
+            builder.HasKey(b => b.Id);
+            builder.HasIndex(b => b.PublicKey).IsUnique();
+
+            builder.Property(b => b.PublicKey).IsRequired().HasMaxLength(56);
+            builder.Property(b => b.SecretKey).IsRequired().HasMaxLength(56);
+            builder.Property(b => b.UserId).IsRequired();
+
+            builder.HasOne(b => b.User)
+                .WithMany(u => u.BlockchainAccounts)
+                .HasForeignKey(b => b.UserId);
+        }
+    }
+}
---
+++ StellarWallet.Infrastructure/Config/UserContactConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using StellarWallet.Domain.Entities;
+
+namespace StellarWallet.Infrastructure.Config
+{
+    public class UserContactConfiguration : IEntityTypeConfiguration<UserContact>
+    {
+        public void Configure(EntityTypeBuilder<UserContact> builder)
+        {
+            builder.HasKey(uc => uc.Id);
+
+            builder.Property(uc => uc.Alias)
+                .IsRequired()
+                .HasMaxLength(50);
+            builder.Property(uc => uc.PublicKey)
+                .IsRequired()
+                .HasMaxLength(56);
+            builder.Property(uc => uc.UserId)
+                .IsRequired();
+
+            builder.HasOne(uc => uc.User)
+                .WithMany(u => u.UserContacts)
+                .HasForeignKey(uc => uc.UserId)
+                .OnDelete(DeleteBehavior.NoAction);
+        }
+    }
+}
