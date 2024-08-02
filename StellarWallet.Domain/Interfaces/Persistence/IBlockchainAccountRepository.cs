eaea40aac832f9ab79cfc0ae8b1d2461c5fce09f
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-08-01 22:12:17 -0300

    Update all the repositories interfaces and move them to the /Persistence folder

---
+++ StellarWallet.Domain/Interfaces/Persistence/IBlockchainAccountRepository.cs
@@ -0,0 +1,8 @@
+﻿using StellarWallet.Domain.Entities;
+
+namespace StellarWallet.Domain.Interfaces.Persistence
+{
+    public interface IBlockchainAccountRepository : IRepository<BlockchainAccount>
+    {
+    }
+}
---
+++ StellarWallet.Domain/Interfaces/Persistence/IUserContactRepository.cs
@@ -0,0 +1,9 @@
+﻿using StellarWallet.Domain.Entities;
+
+namespace StellarWallet.Domain.Interfaces.Persistence
+{
+    public interface IUserContactRepository : IRepository<UserContact>
+    {
+        Task Delete(int id);
+    }
+}
