dec698b2ce3c447a66ead0dc03f17ffffdba3bc5
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-08-01 22:09:34 -0300

    Implementing the UnitOfWork class and its interface IUnitOfWork

---
+++ StellarWallet.Domain/Interfaces/Persistence/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿namespace StellarWallet.Domain.Interfaces.Persistence
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IUserRepository User { get; }
+        IBlockchainAccountRepository BlockchainAccount { get; }
+        IUserContactRepository UserContact { get; }
+        Task Save();
+    }
+}
---
+++ StellarWallet.Infrastructure/Repositories/UnitOfWork.cs
@@ -0,0 +1,33 @@
+﻿using StellarWallet.Domain.Interfaces.Persistence;
+using StellarWallet.Infrastructure.DatabaseConnection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StellarWallet.Infrastructure.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly DatabaseContext _context;
+        public IUserRepository User { get; }
+        public IBlockchainAccountRepository BlockchainAccount { get; }
+        public IUserContactRepository UserContact { get; }
+
+        public UnitOfWork(DatabaseContext context)
+        {
+            _context = context;
+            User = new UserRepository(_context);
+            BlockchainAccount = new BlockchainAccountRepository(_context);
+            UserContact = new UserContactRepository(_context);
+        }
+
+        public void Dispose() => _context.Dispose();
+
+        public async Task Save()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
