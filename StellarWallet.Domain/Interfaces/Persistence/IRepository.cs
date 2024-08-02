be5f863aee96973e374ce285b2f0bc1e311f060b
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-08-01 22:10:02 -0300

    Implement a generic Repository class and its interface IRepository

---
+++ StellarWallet.Domain/Interfaces/Persistence/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StellarWallet.Domain.Interfaces.Persistence
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAll(Expression<Func<T, bool>>? filter = null, Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null, string? includeProperties = null, bool istracking = true);
+        Task<T?> GetById(int id);
+        Task Add(T entity);
+        void Delete(T entity);
+        void Update(T entity);
+    }
+}
---
+++ StellarWallet.Infrastructure/Repositories/Repository.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StellarWallet.Domain.Interfaces.Persistence;
+using StellarWallet.Infrastructure.DatabaseConnection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StellarWallet.Infrastructure.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly DatabaseContext _context;
+        internal DbSet<T> dbSet;
+
+        public Repository(DatabaseContext context)
+        {
+            _context = context;
+            dbSet = _context.Set<T>();
+        }
+
+        public async Task Add(T entity)
+        {
+            await dbSet.AddAsync(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            dbSet.Remove(entity);
+        }
+
+        public async Task<IEnumerable<T>> GetAll(Expression<Func<T, bool>>? filter = null, Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null, string? includeProperties = null, bool istracking = true)
+        {
+            IQueryable<T> query = dbSet;
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            if (includeProperties != null)
+            {
+                foreach (var includeProperty in includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(includeProperty);
+                }
+            }
+
+            if (orderBy != null)
+            {
+                query = orderBy(query);
+            }
+
+            if (!istracking)
+            {
+                query = query.AsNoTracking();
+            }
+
+            return await query.ToListAsync();
+        }
+
+        public async Task<T?> GetById(int id)
+        {
+            return await dbSet.FindAsync(id);
+        }
+
+        public void Update(T entity)
+        {
+            dbSet.Update(entity);
+            _context.SaveChangesAsync();
+        }
+    }
+}
