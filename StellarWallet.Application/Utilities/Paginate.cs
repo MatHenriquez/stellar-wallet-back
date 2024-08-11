cc2864a496af58bc931cab92a6fdeec531928936
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-08-11 19:30:06 -0300

    Add Paginate utility class

---
+++ StellarWallet.Application/Utilities/Paginate.cs
@@ -0,0 +1,22 @@
+﻿namespace StellarWallet.Application.Utilities
+{
+    public static class Paginate
+    {
+        public static List<T> PaginateQuery<T>(List<T> query, int pageNumber, int pageSize)
+        {
+            ValidatePageNumber(pageNumber, GetTotalPages(query.Count(), pageSize));
+            return query.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToList();
+        }
+
+        public static int GetTotalPages(int totalItems, int pageSize)
+        {
+            return (int)Math.Ceiling(totalItems / (double)pageSize);
+        }
+
+        private static void ValidatePageNumber(int pageNumber, int totalPages)
+        {
+            if (pageNumber <= 0 || pageNumber > totalPages)
+                throw new Exception("Invalid page number.");
+        }
+    }
+}
