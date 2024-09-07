be7a8c82915fbfdfc8785326db5b0211a7c0c8ad
Matías Henríquez <matias.henriquez.dev@gmail.com>
2024-09-06 23:21:17 -0300

    Update the database context import in the migration files

--- StellarWallet.Infrastructure/Migrations/20240511204834_UserContactNewPK.Designer.cs
+++ StellarWallet.Infrastructure/Migrations/20240511204834_UserContactNewPK.Designer.cs
@@ -5,7 +5,7 @@ using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-using StellarWallet.Infrastructure.DatabaseConnection;
+using StellarWallet.Infrastructure;
 
 #nullable disable
 
