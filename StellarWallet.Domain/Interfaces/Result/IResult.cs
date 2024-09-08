2e102e27f49dc56963c207e0186d0bf0a6defd6f
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-09-07 23:56:35 -0300

    Add Result class and IResult interface

---
+++ StellarWallet.Domain/Interfaces/Result/IResult.cs
@@ -0,0 +1,11 @@
+﻿using StellarWallet.Domain.Result;
+
+namespace StellarWallet.Domain.Interfaces.Result
+{
+    public interface IResult<TValue, TError>
+    {
+        TValue Value { get; }
+        bool IsSuccess { get; }
+        TError Error { get; }
+    }
+}
---
+++ StellarWallet.Domain/Result/Result.cs
@@ -0,0 +1,23 @@
+﻿using StellarWallet.Domain.Errors;
+using StellarWallet.Domain.Interfaces.Result;
+
+namespace StellarWallet.Domain.Result
+{
+    public class Result<TValue, TError> : IResult<TValue, TError>
+    {
+        public TValue Value { get; }
+        public bool IsSuccess { get; }
+        public TError Error { get; }
+
+        private Result(TValue value, bool isSuccess, TError error)
+        {
+            Value = value;
+            IsSuccess = isSuccess;
+            Error = error;
+        }
+
+        public static Result<TValue, TError> Success(TValue value) => new(value, true, default);
+
+        public static Result<TValue, TError> Failure(TError error) => new(default, false, error);
+    }
+}
