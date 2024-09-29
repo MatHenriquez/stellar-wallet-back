75c5970118121dcf0f24e53b102be35192b89805
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-09-29 16:55:23 -0300

    Add memo parameter to the SendPayment method in the StellarService class and the IBlockchainService interface

--- StellarWallet.Domain/Interfaces/Services/IBlockchainService.cs
+++ StellarWallet.Domain/Interfaces/Services/IBlockchainService.cs
@@ -1,7 +1,7 @@
 ﻿using StellarWallet.Domain.Entities;
-using StellarWallet.Domain.Structs;
-using StellarWallet.Domain.Result;
 using StellarWallet.Domain.Errors;
+using StellarWallet.Domain.Result;
+using StellarWallet.Domain.Structs;
 
 namespace StellarWallet.Domain.Interfaces.Services
 {
@@ -9,7 +9,7 @@ namespace StellarWallet.Domain.Interfaces.Services
     {
         public BlockchainAccount CreateAccount(int userId);
         public AccountKeyPair CreateKeyPair();
-        public Task<Result<bool, DomainError>> SendPayment(string sourceSecretKey, string destinationPublicKey, string amount);
+        public Task<Result<bool, DomainError>> SendPayment(string sourceSecretKey, string destinationPublicKey, string amount, string memo);
         public Task<Result<BlockchainPayment[], DomainError>> GetPayments(string publicKey);
         public Task<Result<bool, DomainError>> GetTestFunds(string publicKey);
         public Task<Result<List<AccountBalances>, DomainError>> GetBalances(string publicKey);
