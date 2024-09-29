bb5e56d1550ed4447bbc6323fd59ef11337baffa
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-09-29 16:53:43 -0300

    Add the Memo attribute to SendPaymentDto

--- StellarWallet.Application/Dtos/Requests/SendPaymentDto.cs
+++ StellarWallet.Application/Dtos/Requests/SendPaymentDto.cs
@@ -2,7 +2,7 @@
 
 namespace StellarWallet.Application.Dtos.Requests
 {
-    public class SendPaymentDto(string destinationPublicKey, decimal amount)
+    public class SendPaymentDto(string destinationPublicKey, decimal amount, string memo)
     {
         [Required(ErrorMessage = "Public key is required")]
         [StringLength(56, MinimumLength = 56, ErrorMessage = "Public key must have 56 characters")]
@@ -10,5 +10,8 @@ namespace StellarWallet.Application.Dtos.Requests
 
         [Range(0.0001, double.MaxValue, ErrorMessage = "Invalid amount")]
         public decimal Amount { get; set; } = amount;
+
+        [StringLength(28, MinimumLength = 1, ErrorMessage = "Memo must have between 1 and 28 characters")]
+        public string? Memo { get; set; } = memo;
     }
 }
