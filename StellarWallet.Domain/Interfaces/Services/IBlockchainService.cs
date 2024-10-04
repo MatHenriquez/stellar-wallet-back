c05a4a90e7a3c8952728c4d21a827c4a86103195
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-10-04 20:56:04 -0300

    Refactor SendPayment method in StellarService and its interface

--- StellarWallet.Domain/Interfaces/Services/IBlockchainService.cs
+++ StellarWallet.Domain/Interfaces/Services/IBlockchainService.cs
@@ -9,7 +9,7 @@ namespace StellarWallet.Domain.Interfaces.Services
     {
         public BlockchainAccount CreateAccount(int userId);
         public AccountKeyPair CreateKeyPair();
-        public Task<Result<bool, DomainError>> SendPayment(string sourceSecretKey, string destinationPublicKey, string amount, string memo);
+        public Task<Result<bool, DomainError>> SendPayment(string sourceSecretKey, string destinationPublicKey, string amount, string assetIssuer, string assetCode, string memo);
         public Task<Result<BlockchainPayment[], DomainError>> GetPayments(string publicKey);
         public Task<Result<bool, DomainError>> GetTestFunds(string publicKey);
         public Task<Result<List<AccountBalances>, DomainError>> GetBalances(string publicKey);
